796a708e0a166db072a5af5cfb5de43d7be821c0
monoliths-uni <monoliths@sina.com>
2021-07-02 11:58:14 +0800

    to solve view

---
+++ src/main/java/top/monoliths/slight_server/kernel/ResponseRulesMap.java
@@ -0,0 +1,20 @@
+package top.monoliths.slight_server.kernel;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * package class
+ */
+public class ResponseRulesMap extends ConcurrentHashMap<String, ResponseRule> {
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append("rules: \n\r");
+        values().forEach(responseRule -> {
+            result.append(responseRule.toString());
+        });
+        result.append("rules end\n\r");
+        result.append("--------------------------------\n\r");
+        return result.toString();
+    }
+}
