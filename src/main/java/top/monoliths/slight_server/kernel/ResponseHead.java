0f74a35e86bb60b3bcd95da953cb83493eb3d4c1
monoliths-uni <monoliths@sina.com>
2021-07-02 10:11:20 +0800

    reconstruct structure

--- src/main/java/top/monoliths/slight_server/kernel/ResponseHead.java
+++ src/main/java/top/monoliths/slight_server/kernel/ResponseHead.java
@@ -6,12 +6,29 @@ package top.monoliths.slight_server.kernel;
  * this method will used to next version
  * 
  * @author monoliths
+ * @since 1.1.0
+ * @version 1.1.0
  */
-
 @SuppressWarnings("unused")
 public class ResponseHead {
-    private String charset;
-    private String head;
+
+    /**
+     * request extension name of file type
+     */
+    private String extensionName;
+
+    /**
+     * if use binary then is true
+     */
     private boolean binary;
-    // TODO complate tag
+
+    /**
+     * encode UTF-8 or binary none
+     */
+    private String charset;
+
+    /**
+     * response data head
+     */
+    private String head;
 }
